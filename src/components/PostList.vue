d570d0a59e31d81cee59ec7707ec3ab22882d24b
Traniel Pride <tranielmp8@gmail.com>
2025-02-15 09:11:12 -0600

    starting blog creation

---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id" >
+      <SinglePost :post="post" />      
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import SinglePost from './SinglePost.vue'
+
+  defineProps({
+    posts: Array
+  })
+
+</script>
+
+<style >
+
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{ post.title }}</h3>
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+  post: Object
+})
+
+ // create a snippet for partial post.body, if you define a variable called props you have to use it here see above how I definedProps
+ const snippet = computed(() => {
+    return props.post.body.substring(0, 100) + '...'
+  })
+
+</script>
+
+<style >
+
+</style>
